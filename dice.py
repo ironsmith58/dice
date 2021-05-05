753fb0a1482bd3866277190e90ce9ac7ecedec64
ironsmith58 <efwolcott@gmail.com>
2021-05-04 23:10:58 -0400

    add roll() to module

--- dice.py
+++ dice.py
@@ -1,11 +1,38 @@
-'''Provide RPG style dice spec parsing and number generation.
-Many games use a random number specification based on a number of, and type of dice to roll, with optional constant to be added to the final number.  e.g. 3D4+2 would mean roll 3 4-sided dice, add the numbers together (sum) and add 2 to the final sum.
+'''
+Provide RPG style dice spec parsing and number generation.
+Many games use a random number specification based on a number of, and type of dice to roll, with an optional 
+constant to be added to the final number.  
+
+e.g. 3D4+2 would mean roll 3 4-sided dice, add the numbers together (sum) and add 2 to the final sum.
+
+A dice spec can be a single int, meaning generate a random number between 1 and int
+or, a range "2-5", meaning generate a number between 2 and 5, this is the same as 1D4+1
+or, 4D8+3, this means roll 4 8-sided dice, sum the rolls and add 3 to the final
+
+the dice rolles can be modified by +, -, *, /.  Although most games only use addition(+) and subtraction(-)
+
 '''
 import random
 import re
 
+def roll(spec, prev={}):
+    if spec in prev:
+        d=prev[spec]
+        return d.roll()
+    if isinstance(spec, Dice):
+        return spec.roll()
+    else:
+        d=Dice()
+        d.parse(spec)
+        prev[spec] = d
+        return d.roll()
+    raise ValueError
+
 class Dice:
-    _dice_re = re.compile('(\d*)[dD](\d*)(.*)')
+    _dice_re = re.compile('^(\d*)[dD](\d*)(.*)$')
+    _range_re = re.compile('^(\d+)-(\d+)$')
+    _int_re = re.compile('^(\d+)$')
+
     def __init__(self, spec=None):
         random.seed()
         self.spec = spec
@@ -29,15 +56,29 @@ class Dice:
         '''Parse dice specification of the form of 3d4+2*100
         Roll 3 6sided dice and add 2 to the sum
         '''
-        if not spec or not len(spec):
-            return
         # if int then gen random number
         if isinstance(spec, int):
             self.dice_max = spec
             return
+        if not spec or not len(spec):
+            return
+        parts = Dice._int_re.match(spec)
+        if parts:
+            self.dice_max = int(spec);
+            return
         #compress spaces
         spec = spec.replace(' ', '')
         self.spec = spec
+        #is this a range spec? goblin htk is 2-5, same as 1d4+1
+        parts = Dice._range_re.match(spec)
+        if parts:
+            parts = parts.groups()
+            self.number_of_dice = 1
+            self.dice_max = int(parts[1]) - int(parts[0]) + 1 # 2-5 := 5-2 = 3 + 1 := 1d4
+            acc = int(parts[0]) - 1 # 2-5 := 2-1 = 1 := +1
+            self.ops.append(("+", acc))
+            return
+        #is this a classic DnD dice spec?
         parts = Dice._dice_re.match(spec)
         if parts:
             parts = parts.groups()
@@ -58,9 +99,15 @@ class Dice:
                     acc = acc * 10 + ord(c) - ord('0')
 
 
-    def roll(self):
+    def roll(self, spec=None):
+        if spec:
+            d=Dice()
+            d.parse(spec)
+            return d.roll()
+
         if not self.spec:
             return 0
+
         dmg = 0
         for x in range(self.number_of_dice):
             dmg = dmg + random.randint(1, self.dice_max)
